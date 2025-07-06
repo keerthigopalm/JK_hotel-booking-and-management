d8177af8bfe4f7b9163ef087ca0b0e5b6a9ffccc
KeerthiGopalavannan <keerthigopalm@gmail.com>
2025-07-06 10:33:41 +0530

    Add guard.js file

--- frontEnd/src/service/guard.js
+++ frontEnd/src/service/guard.js
@@ -1,9 +1,25 @@
+import React from "react";
+import {Navicate,useLocation} from 'react-router-dom';
+import ApiService from './ApiServise';
+
 export const ProtectedRoute = ({element : Component }) => {
+    
     const location = useLocation();
 
     return ApiService.isAthenticated() ? (
         Component
     )  :  (
         <Navigate to="/login" replace state= {{ from: location }}/>
+    );
+};
+
+
+export const AdminRoute = ({ element : component}) => {
+    const location = useLocation();
+
+    return ApiService.isAdmin() ? (
+        component
+    ) : (
+        <Navigate ti="/login" replace state={{ from: location}} />
     );
 };
